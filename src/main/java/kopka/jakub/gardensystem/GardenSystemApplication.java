2a69c75c099db796fb95a0c42f116e661bbb52e7
Jakub Kopka <jakubkopka@int.pl>
2019-08-20 08:47:54 +0200

    Update GardenSystemApplication.java

--- src/main/java/kopka/jakub/gardensystem/GardenSystemApplication.java
+++ src/main/java/kopka/jakub/gardensystem/GardenSystemApplication.java
@@ -19,3 +19,37 @@ public class GardenSystemApplication  extends SpringBootServletInitializer {
         return application.sources(GardenSystemApplication.class);
     }
 }
+
+
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+class SimpleCORSFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig fc) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp,
+                         FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) resp;
+        HttpServletRequest request = (HttpServletRequest) req;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN");
+
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(req, resp);
+        }
+
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+}
